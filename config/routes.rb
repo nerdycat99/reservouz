ab41cb022c5c3b697381ff2d49e97388d5b3d75b
nerdycat99 <info@drysky.co.uk>
2021-03-19 10:04:52 +0100

    updated with AASA file

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_#index'
   get 'about', to: 'static_#about'
+  get '/apple-app-site-association' => 'static#apple_app_site_association'
 end
