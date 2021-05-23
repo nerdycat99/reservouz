541aafcc1a162f4c28dc0fe3b5ec51e8ab898332
nerdycat99 <info@drysky.co.uk>
2021-05-23 16:22:26 +0200

    troubleshooting assets2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,11 +47,11 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
+  
   # ***** NW 23 May 2021 commented out
-  config.assets.compile = false
-
+  # config.assets.compile = false
   config.assets.debug = false
-  # config.assets.compile = true
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
